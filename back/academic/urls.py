850be23c29bfa0577e4ccc91426ebc7ffb52947d
Luan Silva <luan.carmo.708@ufrn.edu.br>
2026-09-09 09:13:15 -0300

    feat: Trabalhando na view de submissão

--- back/academic/urls.py
+++ back/academic/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import QuizCreateView, VisualizeTestView
+from .views import QuizCreateView, VisualizeTestView, SubmitQuizView
 
 urlpatterns = [
     path('prova/criar', QuizCreateView.as_view(), name = 'criar-prova'),
-    path('prova/visualizar/<str:quiz_id>', VisualizeTestView.as_view(), name = 'ver-prova')
+    path('prova/visualizar/<str:quiz_id>/', VisualizeTestView.as_view(), name = 'ver-prova'),
+    path('prova/submeter/<str:quiz_id>/', SubmitQuizView.as_view(), name = 'enviar-prova')
 ]
--- back/academic/views.py
+++ back/academic/views.py
@@ -6,7 +6,7 @@ from bson import ObjectId
 from bson.errors import InvalidId
 from datetime import datetime
 from .serializers import QuizzSerializer,QuizSubmissionSerializer
-from .database import quizzes
+from .database import quizzes, submissions
 
 # Create your views here.
 
@@ -59,3 +59,77 @@ class VisualizeTestView(APIView):
             "prazo_limite": prova["prazo_limite"],
             "questoes": questoes_aluno
         }, status = status.HTTP_200_OK)
+
+#Caso de uso: Entregar prova
+class SubmitQuizView(APIView):
+    def post(self, request, quiz_id):
+        serializer = QuizSubmissionSerializer(data=request.data)
+        prova = None
+
+        if not serializer.is_valid():
+            return Response({"erro": f"Quiz inválido: {serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            obj_id = ObjectId(quiz_id.strip())
+        except (InvalidId, TypeError) as e:
+            return Response({"erro": f"Erro no ID: {str(e)}"}, status = status.HTTP_400_BAD_REQUEST)
+
+        try:
+            prova = quizzes.find_one({"_id": obj_id})
+        except Exception as e:
+            return Response({"erro": "Erro de conexão com o banco.", "detalhes": str(e)}, 
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        discente_id = serializer.validated_data["discente_id"]
+        respostas = serializer.validated_data["respostas_enviadas"]
+
+        total_acertos = 0
+        nota_final = 0.0
+        detalhes_correcao = {}
+
+        gabarito_map = {
+            str(p["id"]): {
+                "correta": str(p.get("gabarito", "")).strip().upper(),
+                "valor": float(p.get("valor_questao", 0))
+            } for p in prova.get("questoes", [])
+        }
+
+        for p_id, resposta_aluno in respostas.items():
+            _id = str(p_id)
+            if _id in gabarito_map:
+                alternativa_correta = gabarito_map[_id]["correta"]
+                valor = gabarito_map[_id]["valor"]
+                acertou = (str(resposta_aluno).strip().upper()) == alternativa_correta
+
+                if acertou:
+                    total_acertos += 1
+                    nota_final += valor
+
+                detalhes_correcao[_id] = {
+                    "resposta_aluno": resposta_aluno,
+                    "acertou": acertou,
+                    "pontos_obtidos": valor if acertou else 0.0
+                }
+
+        submissao_pdu = {
+            "quiz_id": prova.get("_id"),
+            "turma_id": prova.get("class_id"),
+            "discente_id": discente_id,
+            "data_submissao": datetime.now(),
+            "respotas_enviadas": respostas,
+            "detalhes_correcao": detalhes_correcao, 
+            "total_acertos": total_acertos,
+            "total_questoes": len(prova.get("questoes", [])),
+            "nota_obtida": nota_final,
+            "status": "CORRIGIDO"
+        }
+
+        resultado = submissions.insert_one(submissao_pdu)
+
+        return Response({
+            "mensagem": "Avaliação enviada com sucesso",
+            "submissao_id": str(resultado.inserted_id),
+            "nota_final": nota_final,
+            "total_acertos": total_acertos,
+            "total_questoes": len(prova.get("questoes", [])),
+        }, status=status.HTTP_201_CREATED)
